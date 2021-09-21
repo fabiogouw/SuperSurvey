83236a05a31fd80681a295a28dc4f8a9e1fc9330
Fabio Gouw <fabiogouw@gmail.com>
2021-09-21 01:19:27 -0300

    Remove unused dependency

--- src/SuperSurvey.UseCases/CastVoteUseCaseImpl.cs
+++ src/SuperSurvey.UseCases/CastVoteUseCaseImpl.cs
@@ -7,19 +7,14 @@ namespace SuperSurvey.UseCases
 {
     public class CastVoteUseCaseImpl : CastVoteUseCase
     {
-        private readonly PollRepository _pollRepository;
         private readonly VoteCommandRepository _voteRepository;
 
-        public CastVoteUseCaseImpl(PollRepository pollRepository,
-            VoteCommandRepository voteRepository)
+        public CastVoteUseCaseImpl(VoteCommandRepository voteRepository)
         {
-            _pollRepository = pollRepository;
             _voteRepository = voteRepository;
         }
         public async Task CastVote(VoteCommand command)
         {
-            Poll poll = await _pollRepository.GetById(command.PollId);
-            Option option = poll.GetOption(command.SelectedOption);
             await _voteRepository.Save(command);
         }
     }
